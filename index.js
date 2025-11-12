ba665f3885d153b31b6273573242703d7d6e0d15
João Pedro A. Hypólito <jpazhy@gmail.com>
2025-11-12 15:33:12 -0300

    chapter 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { routeHello, routeAPInames } from './routes.js';
 
 // Cria uma instância da aplicação Express
 const app = express()
@@ -7,7 +8,21 @@ const port = 3000;
 
 // Define uma rota GET para '/hello' que retorna 'Hello World!'
 app.get('/hello', function (req, res) {
-    res.send('Hello World!');
+    const response = routeHello(req, res);
+
+    res.send(response);
+})
+
+app.get('/api/names', async function (req, res) {
+    let response;
+
+    try {
+        response = await routeAPInames(req, res);
+    } catch (err) {
+        console.log(err)
+    }
+
+    res.send(response);
 })
 
 // Inicia o servidor na porta especificada
